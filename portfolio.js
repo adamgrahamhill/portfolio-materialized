fde831d930eab07d6aabb27acae2e146e2b6db15
Adam Hill <adam.graham.hill@gmail.com>
2016-08-26 22:16:57 -0400

    Auto-select status line input on page load via JS

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,9 @@
-$(document).ready( function(){
+$(document).ready( function(event){
 	console.log("Hola amigos!");
+	// Status line field /////////
+	// Status line field /////////
+	// auto-select field on page load
+	var statusLine = $('#bot-status-line span')
+	statusLine.focus();
+	statusLine.select();
 })
