d4a5d78ae7953eba737935d986d901aa533cd920
Adam Hill <adam.graham.hill@gmail.com>
2016-08-28 15:25:25 -0400

    Add command line click text for Github and resume, connect change text on activate/deactivate

--- portfolio.js
+++ portfolio.js
@@ -11,8 +11,21 @@ $(document).ready( function(){
 				$('#resume-seg .collapsible-header').click();
 			} else if (textEntered.includes("connect")) {
 				$('#connect-seg .collapsible-header').click();
+			} else if (textEntered.includes("github")) {
+				$('.fa-github').click();
+			} else if (textEntered.includes("adam" || "_hill" || "res.txt")) {
+				$('#google-docs-resume').click();
+				console.log(textEntered)
 			}
 			statusLine.val("");
+		}
+	})
+	$('#connect-seg .collapsible-header').click(function(){
+
+		if ($('#connect-seg .collapsible-header').hasClass("active")) {
+			$('#connect-seg .collapsible-header').text('connect');
+		} else {
+			$('#connect-seg .collapsible-header').text("connecting...")
 		}
 	})
 })
