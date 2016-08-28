aab933c41797ba63717406a3de9dfd647651f1bf
Adam Hill <adam.graham.hill@gmail.com>
2016-08-28 10:33:02 -0400

    Change status line to input tag, auto-select on keydown

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,14 @@
 $(document).ready( function(event){
 	console.log("Hola amigos!");
 	// Status line field /////////
-	// Status line field /////////
 	// auto-select field on page load
-	var statusLine = $('#bot-status-line span')
+	var statusLine = $('#bot-status-line input')
 	statusLine.focus();
 	statusLine.select();
+	$(window).keydown(function(event){
+		statusLine.focus();
+		// statusLine.select();
+		console.log("you pressed a key!");
+
+	})
 })
