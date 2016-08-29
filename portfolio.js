e64b8cb057ecc3ed021a022d1fba16b2654c456f
Adam Hill <adam.graham.hill@gmail.com>
2016-08-29 16:56:14 -0400

    Add typing effect, hide profile pic until section selected

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,10 @@
 $(document).ready( function(){
+	$("#top-status").typed({
+		strings: ["Hello! Welcome to my portfolio site. <br>"
+		 +"Type the name of any section and press ENTER <br>"
+		 +" Or click on a section. "],
+		typeSpeed: 0
+	});
 	var statusLine = $('#bot-status-line input')
 	var timeStamp = function(){
 		var date = new Date();
@@ -10,6 +16,7 @@ $(document).ready( function(){
 		if (event.which === 13) {
 			var textEntered = statusLine.val().toLowerCase();
 			if (textEntered.includes("about")) {
+				$('#me').css("display", "inline");
 				$('#about-seg .collapsible-header').click();
 				$('#history').text(timeStamp() +" $ " + textEntered);
 			} else if (textEntered.includes("resume")){
@@ -37,23 +44,22 @@ $(document).ready( function(){
 				$('#history').text(timeStamp() +" $ " + textEntered);
 			} else if (textEntered.includes("echo")){
 				$('#history').text(statusLine.val().replace("echo",""));
+			} else if (textEntered == "whoami") {
+				$('#history').text('Flynn');
 			}else {
 				$('#history').text("-bash: " + textEntered + ": command not found");
-
 			}
 			statusLine.val("");
 		}
 	})
 	$('#files-list').click(function(){
-
 		if ($('#connect-seg .collapsible-header').hasClass("active")) {
 			$('#connect-seg .collapsible-header').text('connecting...');
 			$('#connect-seg .collapsible-header').addClass("blink");
-
 		} else {
 			$('#connect-seg .collapsible-header').text("connect");
 			$('#connect-seg .collapsible-header').removeClass("blink");
-			
+
 		}
 	})
 })
