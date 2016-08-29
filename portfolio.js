db1b8bf13173512acfa36a6d7ff84628fa9250d1
Adam Hill <adam.graham.hill@gmail.com>
2016-08-28 21:08:08 -0400

    Add history <p> element, style input

--- portfolio.js
+++ portfolio.js
@@ -7,16 +7,34 @@ $(document).ready( function(){
 			var textEntered = statusLine.val().toLowerCase();
 			if (textEntered.includes("about")) {
 				$('#about-seg .collapsible-header').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
 			} else if (textEntered.includes("resume")){
 				$('#resume-seg .collapsible-header').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
 			} else if (textEntered.includes("connect")) {
 				$('#connect-seg .collapsible-header').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
 			} else if (textEntered.includes("github")) {
 				$('.fa-github').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
 			} else if (textEntered.includes("adam_hill") || textEntered.includes("res.doc")) {
 				$('#google-docs-resume').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
 			} else if (textEntered.includes("twitter")) {
 				$('#twitter-link').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
+			} else if (textEntered.includes("linkedin")) {
+				$('#linkedin-link').click();
+			$('#history').text("Adams-Macbook-Pro:portfolio_site $ " + textEntered);
+
+			}else {
+			$('#history').text("-bash: " + textEntered + ": command not found");
+
 			}
 			statusLine.val("");
 		}
@@ -25,8 +43,12 @@ $(document).ready( function(){
 
 		if ($('#connect-seg .collapsible-header').hasClass("active")) {
 			$('#connect-seg .collapsible-header').text('connecting...');
+			$('#connect-seg .collapsible-header').addClass("blink");
+
 		} else {
-			$('#connect-seg .collapsible-header').text("connect")
+			$('#connect-seg .collapsible-header').text("connect");
+			$('#connect-seg .collapsible-header').removeClass("blink");
+			
 		}
 	})
 })
